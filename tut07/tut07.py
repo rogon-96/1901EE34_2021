f40b1683b3c8ff348c3a882ec0370662cfd64184
rogon-96 <motukurumidhunreddy@gmail.com>
2021-11-24 16:02:03 +0530

    Endsem

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -3,39 +3,43 @@
 import os
 from openpyxl import Workbook,load_workbook
 import pandas as pd
-
-def check_feedback(course_feedback_table,rollno,subno,ltp):
-    check_list = [rollno,subno,ltp]
-    try:
-        course_feedback_table.loc[[check_list]]
-    except:
-        return True                                                                                 # returing true if feedback is not given .....same as below
-    return False                                                                                    # returning false if feeback is given for a particular course and feedback_type
+                                                                                   
+def make_map(iterable,key_params,value_params,obj):                      #created_maps
+    for index in range(len(iterable)):
+        temp_row = iterable.iloc[index]
+        key_list = temp_row.get(key_params[-1]) if len(key_params)==1 else tuple(temp_row.get(key) for key in key_params)
+        values_list = temp_row.get(value_params[-1]) if len(value_params)==1 else [temp_row.get(key) for key in value_params]
+        obj[key_list] = values_list
+    return obj
 
 def feedback_not_submitted():
     output_file_name = "course_feedback_remaining.xlsx"
-    course_feedback_table = pd.read_csv("course_feedback_submitted_by_students.csv")[["stud_roll","course_code","feedback_type","id"]].set_index(["stud_roll","course_code","feedback_type"])
-    course_info_table = pd.read_csv("course_master_dont_open_in_excel.csv")[["subno","ltp"]].set_index('subno')
-    course_register = pd.read_csv("course_registered_by_all_students.csv")[["rollno","register_sem","schedule_sem","subno"]]
-    studentinfo = pd.read_csv("studentinfo.csv")[["Name","Roll No","email","aemail","contact"]].drop_duplicates(subset=['Roll No'],keep='first').set_index(["Roll No"])
+    course_feedback = pd.read_csv("course_feedback_submitted_by_students.csv")
+    course_info = pd.read_csv("course_master_dont_open_in_excel.csv")
+    course_register = pd.read_csv("course_registered_by_all_students.csv")
+    student_info = pd.read_csv("studentinfo.csv")
+
+    course_feedback_dict = make_map(course_feedback,["stud_roll","course_code","feedback_type"],["id"],{})
+    course_info_dict = make_map(course_info,['subno'],["ltp"],{})
+    student_info_dict = make_map(student_info,["Roll No"],["Name","email","aemail","contact"],{})
 
     wb = Workbook()            
     ws = wb.active  
     ws.append(["rollno","register_sem","schedule_sem","subno","Name","email","aemail","contact"])
 
-    for ind,row in course_register.iterrows():                                                           # iterating through course_register 
-        rollno,subno= row['rollno'],row['subno']
-        ltp_list= course_info_table.loc[f"{subno}"]["ltp"].split('-')                                    #getting bits 
-        for index,ltp in enumerate(ltp_list):
-            if ltp!='0' and check_feedback(course_feedback_table,rollno,subno,index+1):                  #checking feedback is exists or not 
-                try:                                                                                     # getting student info 
-                    stud_info = studentinfo.loc[f"{rollno}"]
-                except:                                                                                  #for somerollnumbers there are no details in student_info
-                    stud_info = ["NA_IN_STUDENTINFO" for i in range(0,4)]
-                ws.append(list(row)+list(stud_info))                                                        #append ing to worksheet
+    for index in range(len(course_register)):                                                                                               
+        temp_row = course_register.iloc[index]                               #getting every row
+        rollno,subno,register_sem,schedule_sem= temp_row['rollno'],temp_row['subno'],temp_row['register_sem'],temp_row['schedule_sem']
+        ltp_list= course_info_dict.get(subno).split('-')                #getting ltp details for courses                                                       
+        for ind,ltp in enumerate(ltp_list):
+            if ltp!='0' and (rollno,subno,ind+1) not in course_feedback_dict:       # condition for not submitted feedback
+                stud_info = student_info_dict.get(rollno) if rollno in student_info_dict else ["NA_IN_STUDENTINFO" for i in range(0,4)] # for only
+                register_info = [rollno,register_sem,schedule_sem,subno]
+                ws.append(register_info+stud_info)                                                       
                 break
     wb.save('./{}'.format(output_file_name))
-    print("Created Succesfully")
     return
 feedback_not_submitted()
+
+
 
